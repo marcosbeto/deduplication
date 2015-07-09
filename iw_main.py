d0c22c66af99e1c181b9638b4ef17535fcf7e740
Marcos <marcos.oliveira@scalablepath.com>
2015-07-09 11:25:45 -0300

    Changing to download option.

--- iw_main.py
+++ iw_main.py
@@ -5,12 +5,12 @@ class IW_Main(object):
 
     def run(self):
         
-        # Downloader().download_photos();
+        Downloader().download_photos();
         # Image_Processor().create_images_histogram_collection();
         # Image_Processor().create_similar_photos_collection()
         # Image_Processor().create_equals_avisos_collection()
         # Image_Processor().create_raw_equal_avisos()
-        Image_Processor().create_duplicateds_group_collection()
+        # Image_Processor().create_duplicateds_group_collection()
 
 
 if __name__ == '__main__':
