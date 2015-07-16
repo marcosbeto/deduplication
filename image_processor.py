14c3f3105440eebe4ac82c1163f59822fc948b98
Marcos <marcos.oliveira@scalablepath.com>
2015-07-16 19:40:20 -0300

    Small changes[10]

--- image_processor.py
+++ image_processor.py
@@ -15,56 +15,56 @@ class Image_Processor(object):
 
 		self.number_of_files = 0
 
-		array_avisos_online = models.get_all_avisos_online()
+		print "Retrieving all online avisos. Please, wait..."
+		self.array_avisos_online = models.get_all_avisos_online()
+		print "[Ok]"		
 
-		print array_avisos_online
+		def step((ext, self), dir_name, files):
 
-		# def step((ext, self), dir_name, files):
+			download_100x75 = True
 
-		# 	download_100x75 = True
+			aviso_id = dir_name[dir_name.rfind("Constants.LOCAL_DIR_SAVE_PHOTO")+len(Constants.LOCAL_DIR_SAVE_PHOTO)+2:dir_name.rfind("/")]
+			aviso_id = aviso_id.translate(None, "/")
 
-		# 	aviso_id = dir_name[dir_name.rfind("Constants.LOCAL_DIR_SAVE_PHOTO")+len(Constants.LOCAL_DIR_SAVE_PHOTO)+2:dir_name.rfind("/")]
-		# 	aviso_id = aviso_id.translate(None, "/")
+			if "100x75" in dir_name or "1200x1200" in dir_name:
 
-		# 	if "100x75" in dir_name or "1200x1200" in dir_name:
+				dir100x75 = dir_name[:dir_name.rfind("/")] + "/100x75"
+				dir1200x1200 = dir_name[:dir_name.rfind("/")] + "/1200x1200"
 
-		# 		dir100x75 = dir_name[:dir_name.rfind("/")] + "/100x75"
-		# 		dir1200x1200 = dir_name[:dir_name.rfind("/")] + "/1200x1200"
+				# print dir_name
+				# is_avison_online = models.is_aviso_online(aviso_id)
+				# print "after"
 
-		# 		# print dir_name
-		# 		is_avison_online = models.is_aviso_online(aviso_id)
-		# 		# print "after"
+				if int(aviso_id) in self.array_avisos_online:
 
-		# 		if is_avison_online:
+					if "1200x1200" in dir_name:
+						if os.path.isdir(dir100x75):
+							download_100x75 = False
 
-		# 			if "1200x1200" in dir_name:
-		# 				if os.path.isdir(dir100x75):
-		# 					download_100x75 = False
+					if download_100x75:
 
-		# 			if download_100x75:
+						aviso_json = {"id_aviso":aviso_id, "photos":[]}
 
-		# 				aviso_json = {"id_aviso":aviso_id, "photos":[]}
+						for file_name in files:
 
-		# 				for file_name in files:
-
-		# 					if file_name.lower().endswith(ext):
+							if file_name.lower().endswith(ext):
 								
-		# 						try:
-		# 							#generating the histogram and adding it to the json to be added to mongo
-		# 							hist = self.get_histogram(os.path.join(dir_name, file_name)) 
-		# 							hist_json = {"photo_path":dir_name + "/" + file_name, "histogram":json.dumps(hist.tolist())}
-		# 							aviso_json["photos"].append(hist_json)
-		# 						except:
-		# 							pass
+								try:
+									#generating the histogram and adding it to the json to be added to mongo
+									hist = self.get_histogram(os.path.join(dir_name, file_name)) 
+									hist_json = {"photo_path":dir_name + "/" + file_name, "histogram":json.dumps(hist.tolist())}
+									aviso_json["photos"].append(hist_json)
+								except:
+									pass
 
-		# 						if self.number_of_files%100==0:
-		# 							print self.number_of_files
+								if self.number_of_files%100==0:
+									print self.number_of_files
 
-		# 						self.number_of_files +=1
+								self.number_of_files +=1
 
-		# 				models.add_image_histogram(aviso_json)
+						models.add_image_histogram(aviso_json)
 		 
-		# os.path.walk(Constants.LOCAL_DIR_SAVE_PHOTO, step, ('.jpg', self))
+		os.path.walk(Constants.LOCAL_DIR_SAVE_PHOTO, step, ('.jpg', self))
 
 
 	def create_images_histogram_collection(self):
