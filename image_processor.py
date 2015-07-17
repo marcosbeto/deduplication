2ca6f0129ec2fac4b94443cea7988ce0d8e71856
Marcos <marcos.oliveira@scalablepath.com>
2015-07-17 00:53:27 -0300

    Small changes[15]

--- image_processor.py
+++ image_processor.py
@@ -25,6 +25,7 @@ class Image_Processor(object):
 
 			aviso_id = dir_name[dir_name.rfind("Constants.LOCAL_DIR_SAVE_PHOTO")+len(Constants.LOCAL_DIR_SAVE_PHOTO)+2:dir_name.rfind("/")]
 			aviso_id = aviso_id.translate(None, "/")
+			
 			if "100x75" in dir_name or "1200x1200" in dir_name:
 				dir100x75 = dir_name[:dir_name.rfind("/")] + "/100x75"
 				dir1200x1200 = dir_name[:dir_name.rfind("/")] + "/1200x1200"
@@ -33,28 +34,31 @@ class Image_Processor(object):
 				# is_avison_online = models.is_aviso_online(aviso_id)
 				# print "after"
 
-				if int(aviso_id) in self.array_avisos_online:
-					if "1200x1200" in dir_name:
-						if os.path.isdir(dir100x75):
-							download_100x75 = False
+				try:
+					if aviso_id in self.array_avisos_online:
+						if "1200x1200" in dir_name:
+							if os.path.isdir(dir100x75):
+								download_100x75 = False
 
-					if download_100x75:
+						if download_100x75:
 
-						aviso_json = {"id_aviso":aviso_id, "photos":[]}
+							aviso_json = {"id_aviso":aviso_id, "photos":[]}
 
-						for file_name in files:
+							for file_name in files:
 
-							if file_name.lower().endswith(ext):
+								if file_name.lower().endswith(ext):
 
-								try:
-									#generating the histogram and adding it to the json to be added to mongo
-									hist = self.get_histogram(os.path.join(dir_name, file_name)) 
-									hist_json = {"photo_path":dir_name + "/" + file_name, "histogram":json.dumps(hist.tolist())}
-									aviso_json["photos"].append(hist_json)
-								except:
-									pass
+									try:
+										#generating the histogram and adding it to the json to be added to mongo
+										hist = self.get_histogram(os.path.join(dir_name, file_name)) 
+										hist_json = {"photo_path":dir_name + "/" + file_name, "histogram":json.dumps(hist.tolist())}
+										aviso_json["photos"].append(hist_json)
+									except:
+										pass
 
-						models.add_image_histogram(aviso_json)
+							models.add_image_histogram(aviso_json)
+				except:
+					pass
 
 				if self.number_of_files%100==0:
 					print self.number_of_files
