7f77f9227631d626679e64fb14b09000e0747ab9
Marcos <marcos.oliveira@scalablepath.com>
2015-07-23 10:43:01 -0300

    Creating equal avisos collection.[25]

--- models.py
+++ models.py
@@ -247,8 +247,8 @@ class Models(object):
 					if number_of_photos_similar_aviso > 3:
 						number_of_photos_similar_aviso_lt = (90*number_of_photos_similar_aviso)/100
 						number_of_photos_similar_aviso_gt = (100*number_of_photos_similar_aviso)/100
-						print 'number_of_photos_similar_aviso_lt: ' + number_of_photos_similar_aviso_lt 
-						print 'number_of_photos_similar_aviso_gt: ' + number_of_photos_similar_aviso_gt
+						print 'number_of_photos_similar_aviso_lt: ' + str(number_of_photos_similar_aviso_lt) 
+						print 'number_of_photos_similar_aviso_gt: ' + str(number_of_photos_similar_aviso_gt)
 
 
 					if not invalid_images:
