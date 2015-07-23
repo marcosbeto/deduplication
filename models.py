60f64cd2b6978a1e81069888b3d17e59d7887fff
Marcos <marcos.oliveira@scalablepath.com>
2015-07-23 09:53:44 -0300

    Creating equal avisos collection.[17]

--- models.py
+++ models.py
@@ -272,10 +272,12 @@ class Models(object):
 						#did not find any similar id_aviso or the array is empty
 						if not similar_aviso_already_on_array:      
 
-							if number_of_photos_similar_aviso_lt == 0 or number_of_photos_similar_aviso_gt == 0:
-								similar_avisos_support = self.con_mongo.ads_similar.find({"id":similar_id_aviso, "np":number_of_photos_similar_aviso},no_cursor_timeout=False).sort([("id", 1)]).batch_size(100)	                      
-							else:
-								similar_avisos_support = self.con_mongo.ads_similar.find({"id":similar_id_aviso, "np" : { "$gt" :  number_of_photos_similar_aviso_lt, "$lt" : number_of_photos_similar_aviso_gt}},no_cursor_timeout=False).sort([("id", 1)]).batch_size(100)
+							# if number_of_photos_similar_aviso_lt == 0 or number_of_photos_similar_aviso_gt == 0:
+							# 	similar_avisos_support = self.con_mongo.ads_similar.find({"id":similar_id_aviso, "np":number_of_photos_similar_aviso},no_cursor_timeout=False).sort([("id", 1)]).batch_size(100)	                      
+							# else:
+							# 	similar_avisos_support = self.con_mongo.ads_similar.find({"id":similar_id_aviso, "np" : { "$gt" :  number_of_photos_similar_aviso_lt, "$lt" : number_of_photos_similar_aviso_gt}},no_cursor_timeout=False).sort([("id", 1)]).batch_size(100)
+
+							similar_avisos_support = self.con_mongo.ads_similar.find({"id":similar_id_aviso},no_cursor_timeout=False).sort([("id", 1)]).batch_size(100)
 							
 							try:
 								
