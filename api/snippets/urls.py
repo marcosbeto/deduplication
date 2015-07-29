ddd43d1bb9ba61e113f812947c4cfdf2c02b1439
Marcos <marcos.oliveira@scalablepath.com>
2015-07-29 09:56:40 -0300

    Changing url.py.

--- api/snippets/urls.py
+++ api/snippets/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('snippets.views',
     url(r'^duplicated/all/$', 'snippet_list'),
     url(r'^duplicated/aviso/(?P<id>[^\?]+)/$', 'snippet_detail'),
-    url(r'^duplicated/api/all/$', 'snippet_list_api'),
-    url(r'^duplicated/api/aviso/(?P<id>[^\?]+)/$', 'snippet_detail_api'),
+    url(r'^api/duplicated/all/$', 'snippet_list_api'),
+    url(r'^api/duplicated/aviso/(?P<id>[^\?]+)/$', 'snippet_detail_api'),
     url(r'^redirect_to_aviso/(?P<id>[^\?]+)/$', 'redirect_to_aviso'),
 )
