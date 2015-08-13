7196d056d04a692010e8e37040dd939007ba7321
Marcos <marcos.oliveira@scalablepath.com>
2015-08-13 15:45:15 -0300

    adding filters to api.[60]

--- api/snippets/urls.py
+++ api/snippets/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('snippets.views',
-    url(r'^duplicated(?P<filters>(?:\w+/)+)', 'snippet_list'),
+    url(r'^duplicated/(?P<filters>(?:\w+/)+)', 'snippet_list'),
     url(r'^duplicated/aviso/(?P<id>[^\?]+)/$', 'snippet_detail'),
     url(r'^api/duplicated/(?P<filters>(?:\w+/)+)', 'snippet_list_api'),
     url(r'^api/duplicated/aviso/(?P<id>[^\?]+)/$', 'snippet_detail_api'),
