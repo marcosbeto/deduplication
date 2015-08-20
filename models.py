123dbf6287d9f6b5edd0550ea376109768892d30
Marcos <marcos.oliveira@scalablepath.com>
2015-08-20 12:05:07 -0300

    Continue filters implementation.[22]

--- models.py
+++ models.py
@@ -174,7 +174,7 @@ class Models(object):
 
 			done_compare = True
 
-	def add_equal_filtered_add(self, filter_option, aviso, equals_avisos_filtered):
+	def add_equal_filtered_add(filter_option, aviso, equals_avisos_filtered):
 
 		# all_idtipodepropiedad = equals_avisos_filtered["idtipodepropiedad"]
 		all_filters_data = equals_avisos_filtered[filter_option]
