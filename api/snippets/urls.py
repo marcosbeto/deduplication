2da5a8f2764ddf4cf911c9db159b35177c58291c
Marcos <marcos.oliveira@scalablepath.com>
2015-08-20 14:22:39 -0300

    Adding new filters to API.[1]

--- api/snippets/urls.py
+++ api/snippets/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('snippets.views',
     url(r'^duplicated/(?P<filters>(?:\w+/)+)', 'snippet_list'),
     url(r'^duplicated/aviso/(?P<id>[^\?]+)/$', 'snippet_detail'),
     url(r'^api/duplicated/(?P<filters>(?:\w+/)+)', 'snippet_list_api'),
+    url(r'^api/duplicated/filtered/$', 'equals_ads_list_filtered'),
     url(r'^api/duplicated/aviso/(?P<id>[^\?]+)/$', 'snippet_detail_api'),
     url(r'^redirect_to_aviso/(?P<id>[^\?]+)/$', 'redirect_to_aviso'),
 )
