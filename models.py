e5917a91f37b7381c486c0423ac4ef92ae3a853a
Marcos <marcos.oliveira@scalablepath.com>
2015-08-25 23:31:08 -0300

    Implementing new dashboard for deduplication v2.8

--- models.py
+++ models.py
@@ -285,9 +285,8 @@ class Models(object):
 						break
 
 				if len(numbered_filtered_equal_ads) == 0 or not size_already_on_array:
-					number_of_equals_json = {"noe":array_reas_size,"reas":[]}
+					number_of_equals_json = {"noe":array_reas_size,"reas":[],"size":len(equal_aviso.get("reas"))}
 					number_of_equals_json["reas"].append(equal_aviso.get("reas"))
-					print len(equal_aviso.get("reas"))
 
 					self.con_mongo.grouped_number_of_ads_equals.insert(number_of_equals_json)
 					numbered_filtered_equal_ads.append(number_of_equals_json)
