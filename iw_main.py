1601948f1616e5c64cadf61b8ceef48ac192ad94
Marcos <marcos.oliveira@scalablepath.com>
2015-09-03 14:48:45 -0300

    Adding the new avisos to our histogram table[28].

--- iw_main.py
+++ iw_main.py
@@ -21,7 +21,7 @@ class IW_Main(object):
         # Image_Processor().create_images_histogram_from_online_ads() # <---- CURRENT IN USE: USED WITH BACKUP AND CURRENT ONLINE AVISOS
 
     # Step 1.1 (alternative): Cropping and creating histograms from all photos in backup
-        Image_Processor().create_images_histogram_from_online_ads_sql() # <---- CURRENT IN USE: USED WITH BACKUP AND CURRENT ONLINE AVISOS
+        # Image_Processor().create_images_histogram_from_online_ads_sql() # <---- CURRENT IN USE: USED WITH BACKUP AND CURRENT ONLINE AVISOS
 
 #TODO: REMOVE DUPLICATED ID_AVISS
     
@@ -41,7 +41,7 @@ class IW_Main(object):
         # Image_Processor().create_duplicateds_group_collection_new()
 
     # Step 7: Adding some filters to each of equal aviso data 
-        # Filters().create_detailed_repeated_ads_filters()
+        Filters().create_detailed_repeated_ads_filters()
 
     # Step 8: Grouping all the equal ads by its filters
         # Filters().group_repeated_ads_filters()
